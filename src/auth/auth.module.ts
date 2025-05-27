f72c15eda970b682c84f42cc806ace62c098ffd0
cristiannZzzz <cristian.bulla02@gmail.com>
2025-05-27 18:29:33 -0400

    Add Firebase configuration to .env.template and implement LoginDto class

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,20 +2,10 @@ import { Module, forwardRef } from '@nestjs/common';
 import { FirebaseAuthGuard } from './firebase-auth.guard';
 import { RolesGuard } from './roles.guard';
 import { UsersModule } from '../users/users.module';
-<<<<<<< HEAD
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
-
-@Module({
-  imports: [UsersModule, PassportModule, JwtModule],
-  controllers: [AuthController],
-  providers: [AuthService],
-=======
 
 @Module({
   imports: [forwardRef(() => UsersModule)],
   providers: [FirebaseAuthGuard, RolesGuard],
   exports: [FirebaseAuthGuard, RolesGuard],
->>>>>>> fusion
 })
 export class AuthModule {}
